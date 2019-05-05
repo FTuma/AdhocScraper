a9f3eef444c89949baa483944830751faa24a11c
felix-tuma <felix.tuma@outlook.de>
2019-05-05 20:13:16 +0200

    - Added ArivaStockItem to store meta data & download stock prices from from ariva.de

--- adhocScraper/adhocScraper/items.py
+++ adhocScraper/adhocScraper/items.py
@@ -44,3 +44,35 @@ class AdhocNewsItem(Item):
         input_processor=MapCompose(remove_tags, replace_entities, lambda x: x.split()),
         output_processor=Join(),
     )
+
+
+class ArivaStockItem(Item):
+    arivaID = Field(input_processor=Identity(), #MapCompose(lambda v: v.attrib['value']),
+                    output_processor=TakeFirst())
+    exchangeID = Field(input_processor=Identity(), #MapCompose(lambda v: v.attrib['value']),
+                       output_processor=TakeFirst())
+    security_name = Field(
+        input_processor=MapCompose(remove_tags, replace_entities),
+        output_processor=TakeFirst(),
+    )
+    isin = Field(
+        input_processor=Identity(),
+        output_processor=TakeFirst(),
+    )
+    foundingyear = Field(input_processor=Compose(lambda v: v[1] if len(v) == 2 else '', remove_tags),
+                         output_processor=TakeFirst())
+    ticker = Field(input_processor=Compose(lambda v: v[1] if len(v) == 2 else '', remove_tags),
+                   output_processor=TakeFirst())
+    listingdate = Field(input_processor=Compose(lambda v: v[1] if len(v) == 2 else '', remove_tags),
+                        output_processor=TakeFirst())
+    country = Field(input_processor=Compose(lambda v: v[1] if len(v) == 2 else '', remove_tags),
+                    output_processor=TakeFirst())
+    industry = Field(input_processor=Compose(lambda v: v[1] if len(v) == 2 else '', remove_tags),
+                     output_processor=TakeFirst())
+    stocktype = Field(input_processor=Compose(lambda v: v[1] if len(v) == 2 else '', remove_tags),
+                      output_processor=TakeFirst())
+    sector = Field(input_processor=Compose(lambda v: v[1] if len(v) == 2 else '', remove_tags),
+                   output_processor=TakeFirst())
+
+    file_urls = Field()
+    files = Field()
