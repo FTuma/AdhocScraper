fa9fa02924d4c25e8dca16c9a15ec8adf4d69940
felix-tuma <felix.tuma@outlook.de>
2019-07-08 21:01:18 +0200

    WIP: Adding Arctic DB pipeline

--- adhocScraper/adhocScraper/pipelines.py
+++ adhocScraper/adhocScraper/pipelines.py
@@ -5,7 +5,25 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import scrapy
+from arctic import CHUNK_STORE, Arctic
 from scrapy.contrib.pipeline.images import FilesPipeline
+
+
+# from scrapy.exceptions import DropItem
+#
+#
+# class DuplicatesPipeline(object):
+#
+#     def __init__(self):
+#         self.ids_seen = set()
+#
+#     def process_item(self, item, spider):
+#         if item['id'] in self.ids_seen:
+#             raise DropItem("Duplicate item found: %s" % item)
+#         else:
+#             self.ids_seen.add(item['id'])
+#             return item
+
 
 class AdhocscraperPipeline(object):
     def process_item(self, item, spider):
@@ -20,3 +38,30 @@ class ArivaFilePipeline(FilesPipeline):
 
     def file_path(self, request, response=None, info=None):
         return 'isin_%s.csv' % request.meta.get('isin')
+
+
+class ArcticPipeline(object):
+
+    # collection_name = 'scrapy_items'
+
+    def __init__(self, arctic_uri, arctic_store):
+        self.arctic_uri = arctic_uri
+        self.arctic_store = arctic_store
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            arctic_uri=crawler.settings.get('ARCTIC_URI'),
+            arctic_store=crawler.settings.get('ARCTIC_STORE')
+        )
+
+    def open_spider(self, spider):
+        self.client = Arctic(self.arctic_uri)
+        self.db = self.client.initialize_library(self.arctic_store, lib_type=CHUNK_STORE)
+
+    def close_spider(self, spider):
+        pass
+
+    def process_item(self, item, spider):
+        # self.db[self.collection_name].insert_one(dict(item))
+        return item
